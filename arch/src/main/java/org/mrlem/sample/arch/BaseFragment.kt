c69db18c141914a1e3b18dacdad36537792bd39e
Sebastien Guillemin <sebastien.guillemin@gmail.com>
2020-04-21 18:30:09 +0200

    arch: add base fragment too

---
+++ arch/src/main/java/org/mrlem/sample/arch/BaseFragment.kt
@@ -0,0 +1,49 @@
+package org.mrlem.sample.arch
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.CallSuper
+import androidx.fragment.app.Fragment
+
+/**
+ * Base fragment to be extended by feature fragments.
+ *
+ * Provides:
+ * - easy layout declaration
+ * - handy callbacks to categorize init code
+ */
+abstract class BaseFragment : Fragment() {
+
+    protected abstract val layout: Int
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(layout, container, false)
+    }
+
+    @CallSuper
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initViews()
+        initEvents()
+        initObservations()
+    }
+
+    /**
+     * Initialize the views.
+     */
+    protected open fun initViews() {}
+
+    /**
+     * Initialize view event listeners.
+     */
+    protected open fun initEvents() {}
+
+    /**
+     * Initialize view-model state observations.
+     */
+    protected open fun initObservations() {}
+
+}

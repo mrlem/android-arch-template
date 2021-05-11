7a28a155688eb93d8b4819e132aa27cf805224e6
Sebastien Guillemin <sebastien.guillemin@gmail.com>
2021-05-11 21:51:28 +0200

    build: add meven group & version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,6 @@
 rootProject.name = "android-arch"
 
+// list all projects
 include(
     ":arch:app",
     ":arch:domain",
@@ -8,3 +9,8 @@ include(
     ":sample:domain",
     ":sample:data"
 )
+
+// set project names to generate correct artifact ids
+project(":arch:app").name = "android-arch-app"
+project(":arch:domain").name = "android-arch-domain"
+project(":arch:data").name = "android-arch-data"
